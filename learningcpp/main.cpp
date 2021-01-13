5e23d50ec3f43c4c813e62ecbe818bb0cf6c2751
Елдос <eldosmaxutov@macbook-pro-eldos.local>
2021-01-14 00:55:49 +0600

    Initial Commit

---
+++ learningcpp/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  learningcpp
+//
+//  Created by Eldos Maxutov on 14.01.2021.
+//  Copyright © 2021 Eldos Maxutov. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
